c95142353767b6d06658a3b841e1991a020a52fe
hiro08gh <hiro08gh@gmail.com>
2019-11-13 12:32:50 +0900

    [add] eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
