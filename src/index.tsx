f3f1a8226ee4e35ba34e95eebac49fe93f91dd21
hiro08gh <hiro08gh@gmail.com>
2019-12-11 09:09:08 +0900

    [add] google analytics and raven

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import * as Sentry from '@sentry/browser';
+import { history } from './utils/history';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { NODE_ENV } = process.env;
+
+if (NODE_ENV === 'production') {
+  //google analyticsの初期設定
+  ReactGA.initialize(process.env.GOOGLE_ANALYTICS_SECRET);
+
+  history.listen(({ pathname }) => {
+    ReactGA.set({ page: pathname });
+    ReactGA.pageview(pathname);
+  });
+
+  //ravenの初期設定
+  Sentry.init({ dsn: process.env.RAVEN_SECRET });
+}
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
