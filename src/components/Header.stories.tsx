0ca9207e6ec816733ed78a359a127bea1237e27d
hiro08gh <hiro08gh@gmail.com>
2019-12-11 19:13:43 +0900

    [add] storybook

---
+++ src/components/Header.stories.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Header from './Header';
+
+export default { title: 'Header' };
+
+export const withText = () => <button>Hello Button</button>;
