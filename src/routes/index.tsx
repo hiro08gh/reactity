1c85186e69b71bcf26799a98e45b47eab0ce1a46
hiro08gh <hiro08gh@gmail.com>
2019-12-13 08:00:59 +0900

    [add] suspenseでページ毎にコード分割

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import MainLayout from '../components/MainLayout';
-import App from '../pages/App';
-import NotFound from '../pages/NotFound';
+
+const App = lazy(() => import(/* webpackChunkName: 'App' */ '../pages/App'));
+const NotFound = lazy(() => import(/* webpackChunkName: 'NotFound' */ '../pages/NotFound'));
 
 const Root: React.FC = () => {
   return (
     <MainLayout>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Suspense>
     </MainLayout>
   );
 };
