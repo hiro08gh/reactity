e2616affc38293e557e1d7deb73006e25d5f1d8c
hiro08gh <hiro08gh@gmail.com>
2019-12-12 18:01:32 +0900

    [remove] Header.stories.tsx

--- src/components/Header.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import Header from './Header';
-
-export default { title: 'Header' };
-
-export const withText = () => <button>Hello Button</button>;
