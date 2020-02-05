24c0609c482e6edb24a693f90b06ebff32270dcb
hiro08gh <hiro08gh@gmail.com>
2020-02-05 13:09:18 +0900

    remove storybook

--- .storybook/config.ts
+++
@@ -1,9 +0,0 @@
-import { addParameters, configure } from '@storybook/react';
-
-const component = require.context('../src', true, /\.stories\.tsx?$/);
-
-function loadStories() {
-  component.keys().forEach(filename => component(filename));
-}
-
-configure(loadStories, module);
--- src/components/atoms/atoms.stories.tsx
+++
@@ -1,6 +0,0 @@
-//atoms.stories.js
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import addStories from '@storybook/react';
-
-storiesOf('atoms', module).add('with text', () => <h1>Text</h1>);
