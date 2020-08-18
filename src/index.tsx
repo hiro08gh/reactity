ec466b3359cfbbe541d5d002dc6eddc7ff287f55
hiro08gh <hiro08gh@gmail.com>
2020-08-18 14:56:46 +0900

    remove: 不要なパッケージを削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { history } from './utils/history';
 
 import Root from './routes';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Root />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>,
   document.getElementById('root')
 );
