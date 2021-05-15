20cf19671a09968894f4ec25945fde37082d1360
hiro08gh <hiro08gh@gmail.com>
2021-05-15 13:31:54 +0900

    chore: version update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import MainLayout from '../components/MainLayout';
